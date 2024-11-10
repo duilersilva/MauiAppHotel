26dace4933ca47f97308c08e7e28c744979beb6f
duilersilva <duilerdd@gmail.com>
2024-11-09 21:44:20 -0300

    projeto concluido

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,38 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
-        public App()
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte  Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0            },
+            new Quarto()
+            {
+                Descricao = "Suíte Econômica",
+                ValorDiariaAdulto = 25.0,
+                ValorDiariaCrianca = 12.5
+            }
+        };
+
+
+                public App()
         {
             InitializeComponent();
             
@@ -17,7 +47,7 @@
             var window = base.CreateWindow(activationState);
 
             window.Width = 400;
-            window.Height = 800;
+            window.Height = 700;
 
             return window;
         }
